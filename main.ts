90d5228337a6fe79d9fb72a1b3f945b73a8fd74e
ahabhgk <ahabhgk@gmail.com>
2022-06-16 23:30:00 +0800

    add blog

---
+++ main.ts
@@ -0,0 +1,21 @@
+import blog, { ga, redirects } from "https://deno.land/x/blog@0.3.3/blog.tsx";
+
+blog({
+  author: "ahabhgk",
+  title: "ahabhgk",
+  avatar: "avatar.png",
+  avatarClass: "full",
+  links: [
+    { title: "Email", url: "mailto:ahabhgk@gmail.com" },
+    { title: "GitHub", url: "https://github.com/ahabhgk" },
+    { title: "Twitter", url: "https://twitter.com/ahabhgk" },
+  ],
+  middlewares: [
+    // ga("UA-XXXXXXXX-X"),
+    // redirects({
+    //   "/foo": "/my_post",
+    //   // you can skip leading slashes too
+    //   "bar": "my_post2",
+    // }),
+  ],
+});
---
+++ posts/hello_world.md
@@ -0,0 +1,6 @@
+---
+title: Hello world!
+publish_date: 2022-06-16
+---
+
+This is my first blog post!
